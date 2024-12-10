1d6dd2eae9c1e62bde83a611144fcecebbbca394
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2024-12-10 16:56:26 +0100

    test

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 const corsOptions = {
     origin: 'https://weather-app-zeta-inky.vercel.app/', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: false,
   };
   
   app.use(cors(corsOptions));
